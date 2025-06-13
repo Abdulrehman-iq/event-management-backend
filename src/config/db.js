60299b4ea9c06d1928855da1a22e966f4684ace8
Abdulrehman iqbal <abdulrehman.iq@outlook.com>
2025-06-13 10:41:23 +0500

    update mongoDB connection string to use the new database

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,11 @@ const connectDB = async () => {
       throw new Error('MONGODB_URI environment variable is not defined');
     }
 
-    await mongoose.connect(dbURI);
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true
+    });
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
